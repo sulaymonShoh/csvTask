68004682d29c093433db3cdee157e8a6f3407b4d
Sulaymonqori Abdumutallibov <86634428+sulaymonShoh@users.noreply.github.com>
2023-12-06 17:17:11 +0500

    Not done

--- lesson2/reader.py
+++ lesson2/reader.py
@@ -1,40 +1,23 @@
 import os
-import csv
-
-
-class FileIterator:
-    def __init__(self, directory):
-        self.directory = directory
-        self.files = [i for i in os.listdir(self.directory)]
-        self.current_index = 0
-
+class FileReader:
+    def __init__(self, file_path):
+        self.file_path = file_path # accepts directory, not file
+        self.index=0
+        self.files = [f for f in os.listdir(self.file_path)]
+        self.cfile = self.file_path + self.files[self.index]
     def __iter__(self):
+
+        self.file = open(self.cfile)
         return self
-
     def __next__(self):
-        if self.current_index-1 < len(self.files):
-            file_path = os.path.join(self.directory, self.files[self.current_index])
-
-            current_file = open(file_path)
-            data = current_file.readlines()
-            current_file.close()
-
-            self.current_index += 1
-
-            # return file_path.split('.')[0], data
-            return data
-        else:
-            raise StopIteration
+        line = self.file.readline()
+        if not line:
+            self.index += 1
+            self.file.close()
+        return line.strip()
 
 
 if __name__ == '__main__':
-    for data in FileIterator('descriptions'):
-        # with open(f'{name}.csv', 'r+') as newfile:
-        #     obj_writer = csv.writer(newfile)
-        #     obj_writer.writerow(['Name', 'Weight', 'Description'])
-        #
-        #     # Assuming data is a list of lines
-        #     for line in data:
-        #         obj_writer.writerow(line.strip().split(','))
-        #         print(line)
-        print(data)
+    with open('output.csv', 'w') as res:
+        for line in FileReader("descriptions/"):
+            res.write(line)
