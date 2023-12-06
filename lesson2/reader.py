50dd4f21c8b4faba464f9cc608db4fb3e49d0263
Sulaymonqori Abdumutallibov <86634428+sulaymonShoh@users.noreply.github.com>
2023-12-06 15:27:09 +0500

    some cahnges

--- lesson2/reader.py
+++ lesson2/reader.py
@@ -1,18 +1,40 @@
-class FileReader:
-    def __init__(self, file_path):
-        self.file_path = file_path
+import os
+import csv
+
+
+class FileIterator:
+    def __init__(self, directory):
+        self.directory = directory
+        self.files = [i for i in os.listdir(self.directory)]
+        self.current_index = 0
+
     def __iter__(self):
-        self.file = open(file=self.file_path)
         return self
+
     def __next__(self):
-        line  = self.file.readline()
-        if not line:
-            self.file.close()
+        if self.current_index-1 < len(self.files):
+            file_path = os.path.join(self.directory, self.files[self.current_index])
+
+            current_file = open(file_path)
+            data = current_file.readlines()
+            current_file.close()
+
+            self.current_index += 1
+
+            # return file_path.split('.')[0], data
+            return data
+        else:
             raise StopIteration
-        return line.strip()
 
 
 if __name__ == '__main__':
-
-    for line in FileReader("descriptions/001.txt"):
-        print(line)
+    for data in FileIterator('descriptions'):
+        # with open(f'{name}.csv', 'r+') as newfile:
+        #     obj_writer = csv.writer(newfile)
+        #     obj_writer.writerow(['Name', 'Weight', 'Description'])
+        #
+        #     # Assuming data is a list of lines
+        #     for line in data:
+        #         obj_writer.writerow(line.strip().split(','))
+        #         print(line)
+        print(data)
