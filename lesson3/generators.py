ab90fd76ff0f8b3e3cb1d21407648704690ab88f
Sulaymonqori Abdumutallibov <86634428+sulaymonShoh@users.noreply.github.com>
2023-12-08 10:07:53 +0500

    tasks from lesson

---
+++ lesson3/generators.py
@@ -0,0 +1,11 @@
+# def pow2(n):
+#     i = 0
+#     while i <= n:
+#         yield 2 ** i
+#         i+=1
+
+
+# if __name__ == '__main__':
+# number = int(input())
+# for i in pow2(number):
+#     print(i)
