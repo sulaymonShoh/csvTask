bbad7f0abed1ca96aad501aa92ee42efec2d46dc
Sulaymonqori Abdumutallibov <86634428+sulaymonShoh@users.noreply.github.com>
2023-12-14 14:42:14 +0500

    homework for lesson6

---
+++ lesson6/homework.py
@@ -0,0 +1,31 @@
+from threading import Thread
+import requests
+import os
+
+
+def write_image(f, data):
+    with open(f'{f}.jpg', 'wb') as image:
+        image.write(data)
+
+
+def get_image(number):
+    url = f"https://randomuser.me/api/portraits/thumb/men/{number}.jpg"
+
+    response = requests.get(url=url)
+    data = response.content
+
+    write_image(number, data)
+
+
+if __name__ == '__main__':
+    os.mkdir("images")
+    os.chdir("images")
+    threads = []
+    for i in range(1, 100):
+        t = Thread(target=get_image, args=(i,))
+        threads.append(t)
+        t.start()
+
+    for i in threads:
+        i.join()
+
