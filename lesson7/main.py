e648b127c2448cb341c08bdc0affac1baa2cbc18
Sulaymonqori Abdumutallibov <86634428+sulaymonShoh@users.noreply.github.com>
2023-12-15 23:14:37 +0500

    codes from lesson7

--- lesson7/main.py
+++ lesson7/main.py
@@ -3,3 +3,6 @@
 # funksiya ichidan gloabl variable qiymati o'zgartirilmasa muammosiz uni qiymatini ishlatish mumkin
 # qachonki o'zgaruvchiga o'zgrtirish kiritish kerak bo'lsa global orqali chaqirish kerak
 #
+# multiprocessing vs multithreading
+# IOBound - processda global o'zgaruvchi o'zgaradi faqat process ichida, processdan keyin global o'zgarishsiz qoladi
+#
